963efa33b0763af80fbd46d45947c9d76cab40a4
karanr14 <karanramnani201@gmail.com>
2026-07-06 16:20:47 +0530

    added programs, editing and renaming left

---
+++ class 10/AI/Program6.py
@@ -0,0 +1,3 @@
+for num in range(100,110,2):
+    square=num*num
+    print(num, "square is", square)
---
+++ class 10/AI/program 2 - area of a rectangle.py
@@ -0,0 +1,5 @@
+#AREA OF RECTANGLE
+length=12
+breadth=7
+area=length*breadth
+print("Area of the rectangle=", area)
